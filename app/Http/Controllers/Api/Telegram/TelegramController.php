a9dd3585a4e65e25cd9e9f50507faebb9672e183
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-05-05 09:05:52 +0700

    ganti logic

--- app/Http/Controllers/Api/Telegram/TelegramController.php
+++ app/Http/Controllers/Api/Telegram/TelegramController.php
@@ -33,6 +33,34 @@ class TelegramController extends Controller
         return $response;
     }
 
+    public function greeting(){
+        $time = time();
+        $hour = date('G',$time);
+        $greeting = "Hallo";
+        if ($hour>=0 && $hour<=11)
+        {
+            $greeting = "Selamat Pagi :)";
+        }
+        elseif ($hour >=12 && $hour<=14)
+        {
+            $greeting = "Selamat Siang :) ";
+        }
+        elseif ($hour >=15 && $hour<=17)
+        {
+            $greeting = "Selamat Sore :) ";
+        }
+        elseif ($hour >=17 && $hour<=18)
+        {
+            $greeting = "Selamat Petang :) ";
+        }
+        elseif ($hour >=19 && $hour<=23)
+        {
+            $greeting = "Selamat Malam :) ";
+        }
+
+        return $greeting;
+    }
+
     public function handleRequest()
     {
         $updates = Telegram::getWebhookUpdates();
@@ -42,43 +70,16 @@ class TelegramController extends Controller
         $this->firstname = $updates->getMessage()->getFrom()->getFirstName();
         $this->lastname = $updates->getMessage()->getFrom()->getLastName();
         $this->text = $updates->getMessage()->getText();
-
-        if($this->text == '/start'){
-            $this->sendMessage($this->startMessage());
-        }
-        
-        if($this->text == 'hi'){
-            $time = time();
-            $hour = date('G',$time);
-            $greeting = "Hallo";
-            if ($hour>=0 && $hour<=11)
-            {
-                $greeting = "Selamat Pagi :)";
-            }
-            elseif ($hour >=12 && $hour<=14)
-            {
-                $greeting = "Selamat Siang :) ";
-            }
-            elseif ($hour >=15 && $hour<=17)
-            {
-                $greeting = "Selamat Sore :) ";
-            }
-            elseif ($hour >=17 && $hour<=18)
-            {
-                $greeting = "Selamat Petang :) ";
-            }
-            elseif ($hour >=19 && $hour<=23)
-            {
-                $greeting = "Selamat Malam :) ";
-            }
-            $random = rand(0,1);
-            if($random == 0){
-                $this->sendMessage('hello '.$this->username);
-            }else{
-                $this->sendMessage($greeting.$this->firstname.' '.$this->lastname);
-            }
-        }else{
-            $this->sendMessage('sayang sekali '.$this->username.' perintah tersebut masih belum saya pahami :(, update selanjutnya menerapkan NLP disini');
+        $text = strtolower($this->text);
+        switch ($text) {
+            case '/start':
+                $this->sendMessage($this->startMessage());
+                break;
+            case 'hi':
+                $this->sendMessage($this->greeting().$this->firstname.' '.$this->lastname);
+            default:
+                $this->sendMessage('sayang sekali '.$this->username.' perintah tersebut masih belum saya pahami :(, update selanjutnya menerapkan NLP disini');
+                break;
         }
     }
 }
