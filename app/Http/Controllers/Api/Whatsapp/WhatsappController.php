b67854a32149f8b4f7b535fda4ac3f86bc433346
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-06-24 06:43:41 +0700

    test wa api

--- app/Http/Controllers/Api/Whatsapp/WhatsappController.php
+++ app/Http/Controllers/Api/Whatsapp/WhatsappController.php
@@ -16,29 +16,14 @@ class WhatsappController extends Controller
         $pick = rand(1,5);
         if (!in_array($body, [1,2,3,4,5])) {
             $twiml->message("Hiya! I'm thinking of a number between 1 and 5 - try to guess it!");
+            return $this->xmlResponse($twiml);
         } elseif ($body == $pick) {
             $twiml->message("Yes! You guessed it!");
+            return $this->xmlResponse($twiml);
         } else {
             $twiml->message("Nope, it was actually $pick - Pick a new number to play again!");
+            return $this->xmlResponse($twiml);
         }
-
-        print $twiml;
-
-        // Find your Account Sid and Auth Token at twilio.com/console
-        // DANGER! This is insecure. See http://twil.io/secure
-        // $sid    = getenv('TWILIO_ACCOUNT_SID');
-        // $token  = getenv('TWILIO_AUTH_TOKEN');
-        // $twilio = new Client($sid, $token);
-
-        // $message = $twilio->messages
-        //                 ->create("whatsapp:+6282257906642", // to
-        //                         array(
-        //                             "from" => "whatsapp:+14155238886",
-        //                             "body" => "Hello there!"
-        //                         )
-        //                 );
-
-        // print($message->sid);
     }
 
     private function xmlResponse($twiml)
