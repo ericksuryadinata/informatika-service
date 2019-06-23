793743e155dfa54c7beda7d117915e1924c0b933
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-06-24 06:48:04 +0700

    response

--- app/Http/Controllers/Api/Whatsapp/WhatsappController.php
+++ app/Http/Controllers/Api/Whatsapp/WhatsappController.php
@@ -13,17 +13,8 @@ class WhatsappController extends Controller
     {
         $twiml = new Twiml;
         $body = $request->input('body');
-        $pick = rand(1,5);
-        if (!in_array($body, [1,2,3,4,5])) {
-            $twiml->message("Hiya! I'm thinking of a number between 1 and 5 - try to guess it!");
-            return $this->xmlResponse($twiml);
-        } elseif ($body == $pick) {
-            $twiml->message("Yes! You guessed it!");
-            return $this->xmlResponse($twiml);
-        } else {
-            $twiml->message("Nope, it was actually $pick - Pick a new number to play again!");
-            return $this->xmlResponse($twiml);
-        }
+        $twiml->message($body);
+        return $this->xmlResponse($twiml);
     }
 
     private function xmlResponse($twiml)
