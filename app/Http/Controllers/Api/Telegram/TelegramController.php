90fd34035c7025672205b146542afea165a44039
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-07-18 13:18:06 +0700

    telegram update

--- app/Http/Controllers/Api/Telegram/TelegramController.php
+++ app/Http/Controllers/Api/Telegram/TelegramController.php
@@ -37,26 +37,6 @@ class TelegramController extends Controller
         return $response;
     }
 
-    public function greeting()
-    {
-        $time = time();
-        $hour = date('G', $time);
-        $greeting = "Hallo";
-        if ($hour>=0 && $hour<=11) {
-            $greeting = "Selamat Pagi :) ";
-        } elseif ($hour >=12 && $hour<=14) {
-            $greeting = "Selamat Siang :) ";
-        } elseif ($hour >=15 && $hour<=17) {
-            $greeting = "Selamat Sore :) ";
-        } elseif ($hour >=17 && $hour<=18) {
-            $greeting = "Selamat Petang :) ";
-        } elseif ($hour >=19 && $hour<=23) {
-            $greeting = "Selamat Malam :) ";
-        }
-
-        return $greeting;
-    }
-
     public function webhook()
     {
         $updates = Telegram::getWebhookUpdates();
@@ -67,28 +47,21 @@ class TelegramController extends Controller
         $this->lastname = $updates->getMessage()->getFrom()->getLastName();
         $this->text = $updates->getMessage()->getText();
         $text = strtolower($this->text);
-
-        switch ($text) {
-            case '/start':
-                $this->sendMessage($this->startMessage());
-                break;
-            case 'hi':
-                $this->sendMessage($this->greeting().$this->firstname.' '.$this->lastname);
-                break;
-            default:
-                $this->sendMessage('sayang sekali '.$this->username.' perintah tersebut masih belum saya pahami :(,'.PHP_EOL.'update selanjutnya menerapkan NLP disini', true);
-                break;
+        if($text === '/start'){
+            $this->sendMessage($this->startMessage());
         }
+
+        $message = $this->extract($text);
+        $this->sendMessage($message,TRUE);
     }
 
-    public function extract()
+    public function extract($body)
     {
-         // Send a GET request to: http://www.foo.com/bar
-        $response = Curl::to(getenv('ENDPOINT_NLP').'data/extraction')
-                        ->withData(array('sentence' => 'lokasi pak aher'))
-                        ->post();
+        $response = Curl::to('http://127.0.0.1:3333/api/v1/data/extraction')
+            ->withData(array('sentence' => $body))
+            ->post();
         $response = json_decode($response);
-        $reply = $response->result->answer;
-        return response()->json($reply, 200);
+        $reply = $response->result;
+        return $reply;
     }
 }
