de053affc71a9d6f42436a13376f141de7374433
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-08-03 05:00:05 +0700

    route buat ngetest

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,25 @@ Route::get('/',function(){
     return 'Hello from api';
 });
 
-Route::namespace('Telegram')->prefix('telegram')->group(function(){
-
-    Route::post(env('TELEGRAM_BOT_TOKEN') . '/webhook','TelegramController@webhook');
-    Route::post('extract','TelegramController@extract');
+Route::namespace('Telegram')->prefix('telegram')->name('telegram.')->group(function(){
+//env('TELEGRAM_BOT_TOKEN')
+    Route::post(env('TELEGRAM_BOT_TOKEN') . '/webhook','TelegramController@webhook')->name('webhook');
+    Route::post('extract','TelegramController@extract')->name('extract');
     Route::prefix('settings')->group(function(){
-        Route::get('get-me','SettingsController@getMe');
-        Route::get('set-web-hook','SettingsController@setWebHook');
+        Route::get('get-me','SettingsController@getMe')->name('get-me');
+        Route::get('set-web-hook','SettingsController@setWebHook')->name('set-webhook');
     });
 
 });
 
-Route::namespace('Whatsapp')->prefix('whatsapp')->group(function(){
-    Route::post(env('TWILIO_ACCOUNT_SID') . '/webhook','WhatsappController@webhook');
-    Route::get('extract/{body}','WhatsappController@extract');
+Route::namespace('Whatsapp')->prefix('whatsapp')->name('whatsapp.')->group(function(){
+//env('TWILIO_ACCOUNT_SID')
+    Route::post(env('TWILIO_ACCOUNT_SID'). '/webhook','WhatsappController@webhook')->name('webhook');
+    Route::get('extract/{body}','WhatsappController@extract')->name('extract');
 });
 
-Route::namespace('UnitTesting')->prefix('unit-testing')->group(function(){
-    Route::get('/', 'UnitTestingController@index');
-    Route::get('/generate', 'UnitTestingController@generate');
+Route::namespace('UnitTesting')->prefix('unit-testing')->name('unit-testing.')->group(function(){
+    Route::get('/', 'UnitTestingController@index')->name('index');
+    Route::get('/generate', 'UnitTestingController@generate')->name('generate');
 });
 
